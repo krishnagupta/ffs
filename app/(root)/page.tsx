a591e3fd58c341dcd96abd4e282f2edda7a8bdc6
Krishna Gupta <krishna_gupta@epam.com>
2025-02-18 16:39:32 +0530

    added logic for end to end

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import {StartupTypeCard} from "@/components/StartupCard";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {auth} from "@/auth";
 
 export default async function Home({
   searchParams,
@@ -6,6 +11,13 @@ export default async function Home({
   searchParams: Promise<{query?: string}>;
 }) {
   const query = (await searchParams).query;
+  const params = {search: query || null};
+
+  const session = await auth();
+
+  console.log(session?.id);
+
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
   return (
     <>
@@ -20,6 +32,23 @@ export default async function Home({
         </p>
         <SearchForm query={query} />
       </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Image from "next/image";
 import {auth} from "@/auth";
 import {signOut, signIn} from "next-auth/react";
+import {BadgePlus, LogOut} from "lucide-react";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
@@ -18,7 +20,8 @@ const Navbar = async () => {
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="mx-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
               <form
@@ -28,11 +31,20 @@ const Navbar = async () => {
                   await signOut({redirectTo: "/"});
                 }}
               >
-                <button type="submit">Logout</button>
+                <button type="submit">
+                  <span className="mx-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
               </form>
 
               <Link href={`/user/${session?.user?.id}`}>
-                <span>{session?.user?.name}</span>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
